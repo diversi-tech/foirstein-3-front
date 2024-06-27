b9289398cb5b9fa2fd9349c2da6454a5e8af8c42
19Daniela <bdnyl5056@gmail.com>
2024-06-27 12:41:23 +0300

    changeStyleAndDisplayTags

--- src/components/form/mediaTable.jsx
+++ src/components/form/mediaTable.jsx
@@ -1,4 +1,4 @@
-import  { useState } from 'react';
+import { useState } from 'react';
 import { observer } from 'mobx-react-lite';
 import mediaStore from '../../store/mediaStore';
 import {
@@ -17,11 +17,13 @@ import {
     Button,
     useMediaQuery,
     useTheme,
-    Stack, Chip
+    Chip,
+    Stack
 } from '@mui/material';
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
 import UpdateDialog from '../update/update';
+import tagStore from '../../store/tagStore';
 
 const MediaTable = observer(() => {
     const [deleteItem, setDeleteItem] = useState(null);
@@ -29,8 +31,8 @@ const MediaTable = observer(() => {
     const [editedItem, setEditedItem] = useState(null);
     const [editOpen, setEditOpen] = useState(false);
 
-    const theme = useTheme();//רספונסיבי
-    const fullScreen = useMediaQuery(theme.breakpoints.down('sm'));//רספונסיבי
+    const theme = useTheme();
+    const fullScreen = useMediaQuery(theme.breakpoints.down('sm'));
 
     const handleDelete = (item) => {
         setDeleteItem(item);
@@ -58,86 +60,90 @@ const MediaTable = observer(() => {
 
     return (
         <>
-            <TableContainer component={Paper} style={{direction:"rtl"}}>
-                <Table>
-                    <TableHead>
-                        <TableRow>
-                            <TableCell align="center">כותרת</TableCell>
-                            <TableCell align="center">תיאור</TableCell>
-                            <TableCell align="center">מדף/קובץ</TableCell>
-                            <TableCell align="center">פעולה</TableCell>
-                            <TableCell align="center">תגית</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {mediaStore.mediaList.map((item) => (
-                            <TableRow key={item.id}>
-                                <TableCell align="center">{item.title}</TableCell>
-                                <TableCell align="center">{item.description}</TableCell>
-                                {/* <TableCell align="center">{item.tag}</TableCell> */}
-                                {/* {mediaStore.mediaList.map()}    צריך לשלוף מתוך */}
-                                <TableCell align="center">{item.shelf}</TableCell>
-                                <TableCell>
-                                    <IconButton
-                                        color="primary"
-                                        onClick={() => handleClickEdit(item)}
-                                    >
-                                        <EditIcon />
-                                    </IconButton>
-                                    <IconButton
-                                        color="secondary"
-                                        onClick={() => handleDelete(item)}
-                                        style={{ marginLeft: '10px' }}
-                                    >
-                                        <DeleteIcon />
-                                    </IconButton>
-                                </TableCell>
-                                <Stack direction="row" spacing={1}>
-                                   <Chip label="תגית" color="primary" variant="outlined" />
-                                   {/* <Chip label="success" color="success" variant="outlined" /> */}
-                                </Stack>
+            <div style={{width: "100%", height: "25%", marginTop: "0%"}}>
+                <h2 style={{ textAlign: "center" }}>רשימת קבצים</h2>
+                <TableContainer component={Paper} style={{ marginTop: "0%", direction: 'rtl' , width: "100vw"}}>
+                    <Table>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell align="center" style={{ fontWeight: 'bold', fontSize: '1.2em', padding: '12px' }}>כותרת</TableCell>
+                                <TableCell align="center" style={{ fontWeight: 'bold', fontSize: '1.2em', padding: '12px' }}>תיאור</TableCell>
+                                <TableCell align="center" style={{ fontWeight: 'bold', fontSize: '1.2em', padding: '12px' }}>מדף/קובץ</TableCell>
+                                <TableCell align="center" style={{ fontWeight: 'bold', fontSize: '1.2em', padding: '12px' }}>פעולה</TableCell>
+                                <TableCell align="center" style={{ fontWeight: 'bold', fontSize: '1.2em', padding: '12px' }}>תגית</TableCell>
                             </TableRow>
-                        ))}
-                    </TableBody>
-                </Table>
-            </TableContainer>
+                        </TableHead>
+                        <TableBody>
+                            {mediaStore.mediaList.map((item) => (
+                                <TableRow key={item.id}>
+                                    <TableCell align="center">{item.title}</TableCell>
+                                    <TableCell align="center">{item.description}</TableCell>
+                                    <TableCell align="center">{item.shelf}</TableCell>
+                                    <TableCell align='center'>
+                                        <IconButton
+                                            color="primary"
+                                            onClick={() => handleClickEdit(item)}
+                                        >
+                                            <EditIcon />
+                                        </IconButton>
+                                        <IconButton
+                                            color="secondary"
+                                            onClick={() => handleDelete(item)}
+                                            style={{ marginLeft: '10px' }}
+                                        >
+                                            <DeleteIcon />
+                                        </IconButton>
+                                    </TableCell>
+                                    <TableCell style={{ minWidth: '200px', overflowX: 'auto' }}>
+                                        <Stack direction="row" spacing={1} style={{ flexWrap: 'nowrap', overflowX: 'auto' }}>
+                                            {tagStore.tagList.map((tag) => (
+                                                <Chip key={tag.id} label={tag.name} color="primary" variant="outlined" />
+                                            ))}
+                                        </Stack>
+                                    </TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
 
-            <Dialog
-                open={deleteOpen}
-                onClose={handleClose}
-                fullScreen={fullScreen}
-            >
-                <DialogTitle>אישור מחיקה</DialogTitle>
-                <DialogContent>
-                    <p>האם אתה בטוח שברצונך למחוק את הפריט הזה?</p>
-                </DialogContent>
-                <DialogActions>
-                    <Button onClick={handleClose} color="primary">
-                        ביטול
-                    </Button>
-                    <Button onClick={confirmDelete} color="secondary">
-                        מחיקה
-                    </Button>
-                </DialogActions>
-            </Dialog>
-
-            {editedItem && (
                 <Dialog
-                    open={editOpen}
+                    open={deleteOpen}
                     onClose={handleClose}
                     fullScreen={fullScreen}
                 >
-                    <DialogTitle>עריכת מדיה</DialogTitle>
+                    <DialogTitle>אישור מחיקה</DialogTitle>
                     <DialogContent>
-                        <UpdateDialog mediaItem={editedItem} onClose={handleClose} />
+                        <p>האם אתה בטוח שברצונך למחוק את הפריט הזה?</p>
                     </DialogContent>
                     <DialogActions>
                         <Button onClick={handleClose} color="primary">
                             ביטול
                         </Button>
+                        <Button onClick={confirmDelete} color="secondary">
+                            מחיקה
+                        </Button>
                     </DialogActions>
                 </Dialog>
-            )}
+
+                {editedItem && (
+                    <Dialog
+                        open={editOpen}
+                        onClose={handleClose}
+                        fullScreen={fullScreen}
+                    >
+                        <DialogTitle>עריכת מדיה</DialogTitle>
+                        <DialogContent>
+                            <UpdateDialog mediaItem={editedItem} onClose={handleClose} />
+                        </DialogContent>
+                        <DialogActions>
+                            <Button onClick={handleClose} color="primary">
+                                ביטול
+                            </Button>
+                        </DialogActions>
+                    </Dialog>
+                )}
+            </div>
         </>
     );
 });
