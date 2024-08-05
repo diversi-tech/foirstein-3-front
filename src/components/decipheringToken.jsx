0d8154470c29d5f5329e83725fc8a521f0aa0e45
רחלי <R0583273805@gmail.com>
2024-08-06 02:58:26 +0300

    no message

--- src/components/decipheringToken.jsx
+++ src/components/decipheringToken.jsx
@@ -62,9 +62,7 @@ export const getRoleFromToken = () => {
 
 
   export const validateToken = async () => {
-    debugger
-    
-    //const token = sessionStorage.getItem('jwt');
+   
     const token = getCookie('jwt');
     console.log(`token!!!!!!!!!`,token);
     if (!token) 
