a943b78757711dc1c663a5367844493eda647cee
DESKTOP-5Q9K2NL\user1 <chay6865@gmail.com>
2024-08-07 11:11:11 +0300

    step 3 in the table

--- src/components/item/BorrowingItemsList.jsx
+++ src/components/item/BorrowingItemsList.jsx
@@ -3,7 +3,7 @@ import { DataGrid } from '@mui/x-data-grid';
 import { useEffect, useState } from 'react';
 import { observer } from "mobx-react-lite";
 import borrowingStore from '../../store/borrowingStore';
-import { Modal, Box } from '@mui/material';
+import { Modal, Box,Tabs,Grid,Tab } from '@mui/material';
 import Button from '@mui/material/Button';
 import CallMissedOutgoingIcon from '@mui/icons-material/CallMissedOutgoing';
 import { IconButton, Typography } from '@mui/material';
@@ -11,38 +11,13 @@ import Tooltip from '@mui/material/Tooltip';
 import CategoryIcon from '@mui/icons-material/Category';
 import MenuBookRoundedIcon from "@mui/icons-material/MenuBookRounded";
 import TextSnippetRoundedIcon from "@mui/icons-material/TextSnippetRounded";
-
-const localeText = {
-  columnMenuSortAsc: "מיון לפי סדר עולה",
-  columnMenuSortDesc: "מיון לפי סדר יורד",
-  columnMenuFilter: "סינון",
-  columnMenuHideColumn: "הסתר עמודה",
-  columnMenuUnsort: "בטל מיון",
-  noRowsLabel: "אין פריטים להצגה",
-  columnMenuManageColumns: "ניהול עמודות",
-  filterPanelAddFilter: "הוסף מסנן",
-  filterPanelDeleteIconLabel: "מחק",
-  filterPanelLinkOperator: "אופרטור לוגי",
-  filterPanelOperators: "אופרטור",
-  filterPanelOperatorAnd: "וגם",
-  filterPanelOperatorOr: "או",
-  filterPanelColumns: "עמודות",
-  filterPanelInputLabel: "ערך",
-  filterPanelInputPlaceholder: "סנן ערך",
-  filterOperatorContains: "מכיל",
-  filterOperatorEquals: "שווה",
-  filterOperatorStartsWith: "מתחיל ב",
-  filterOperatorEndsWith: "מסתיים ב",
-  filterOperatorIs: "הוא",
-  filterOperatorNot: "אינו",
-  filterOperatorAfter: "אחרי",
-  filterOperatorOnOrAfter: "ב או אחרי",
-  filterOperatorBefore: "לפני",
-  filterOperatorOnOrBefore: "ב או לפני",
-  filterOperatorIsEmpty: "ריק",
-  filterOperatorIsNotEmpty: "אינו ריק",
-  filterOperatorIsAnyOf: "הוא אחד מ",
-};
+import CircularProgress from "@mui/material/CircularProgress";
+import { CacheProvider } from "@emotion/react";
+import { cacheRtl } from "../tag/fields_rtl";
+import ItemSearch from "./item-search";
+import { TypeEnum } from "../Enums";
+import { localeText } from './item-list';
+import { toJS } from 'mobx';
 
 const keyToHebrew = {
   id: "מספר זיהוי",
@@ -100,102 +75,230 @@ const StudentDetailsModal = ({ open, handleClose, student }) => (
 
 );
 
-const DataTable = observer(() => {
+const BorrowingTable = observer(() => {
+  const [borrowingItems,setBorrowingItems]=useState([]);
+  const [filterType, setFilterType] = useState("all");
+  const [filteredItems, setFilteredItems] = useState([]);
   const [librarianDetails, setLibrarianDetails] = useState(null);
   const [studentDetails, setStudentDetails] = useState(null);
-  const [columns, setColumns] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     setLoading(true); 
+  //     await borrowingStore.fetchBorrowing();
+  //     setLoading(false); 
+  //   };
+  
+  //   fetchData();
+  // }, []);
+  // useEffect(()=>{
+  //  setBorrowingItems(borrowingStore.getborrowingList)
+  
+  // },[borrowingStore.borrowingList,filterType])
+    
+  // useEffect(()=>{
+  //   setFilteredItems(filterItems());
+  // },[borrowingItems])
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      await borrowingStore.fetchBorrowing();
+      setLoading(false);
+    };
+
+    fetchData();
+  }, [borrowingStore]);
 
   useEffect(() => {
-    borrowingStore.fetchBorrowing();
-  }, []);
-
-  useEffect(() => {
-    if (borrowingStore.borrowingList.length > 0) {
-      const newColumns = [
-        { field: 'id', headerName: keyToHebrew['id'], flex: 1, headerAlign: 'center', align: 'center' },
-        { field: 'date', headerName: keyToHebrew['date'], flex: 1, headerAlign: 'center', align: 'center' },
-        { field: 'itemTitle', headerName: keyToHebrew['itemTitle'], flex: 1, headerAlign: 'center', align: 'center' },
-        {
-          field: 'itemType', headerName: keyToHebrew['itemType'],
-          renderCell: (params) => {
-            switch (params.row.itemType) {
-              case 'book': return <MenuBookRoundedIcon sx={{ color: "#0D1E46" }}/>
-              case 'file': return <TextSnippetRoundedIcon sx={{ color: "#0D1E46" }}/>
-              case 'physical': <CategoryIcon sx={{ color: "#0D1E46" }}/>
-            }
-          },
-          flex: 1,
-          headerAlign: 'center',
-          align: 'center'
-        },
-        {
-          field: 'StudentFullName',
-          headerName: keyToHebrew['StudentFullName'],
-          renderCell: (params) => {
-            const fname = params.row.studentFname || '';
-            const sname = params.row.studentSname || '';
-            return (
-              <div style={{ display: 'flex', alignItems: 'center', position: 'relative' }}>
-                <span style={{ marginRight: 40 }}>{`${fname} ${sname}`.trim()}</span>
-                <Tooltip title="לפרטים נוספים" arrow>
-                  <IconButton
-                    onClick={() => setStudentDetails(params.row)}
-                    style={{ position: 'absolute', right: 0 }}
-                  >
-                    <CallMissedOutgoingIcon />
-                  </IconButton>
-                </Tooltip>
-              </div>
-            );
-          },
-          flex: 1,
-          headerAlign: 'center',
-          align: 'center'
-        },
-        {
-          field: 'LibrarianFullName',
-          headerName: keyToHebrew['LibrarianFullName'],
-          renderCell: (params) => {
-            const fname = params.row.librarianFname || '';
-            const sname = params.row.librarianSname || '';
-            return (
-              <div style={{ display: 'flex', alignItems: 'center', position: 'relative' }}>
-                <span style={{ marginRight: 40 }}>{`${fname} ${sname}`.trim()}</span>
-                <Tooltip title="לפרטים נוספים" arrow>
-                  <IconButton
-                    onClick={() => setLibrarianDetails(params.row)}
-                    style={{ position: 'absolute', right: 0 }}
-                  >
-                    <CallMissedOutgoingIcon />
-                  </IconButton>
-                </Tooltip>
-              </div>
-            );
-          },
-          flex: 1,
-          headerAlign: 'center',
-          align: 'center'
-        },
-        { field: 'amount', headerName: keyToHebrew['amount'],
-        renderCell: (params) => {
-         if(!params.row.amount)
-         {
-          return 'לא מוגדרת כמות'
-         }
-        },
-         flex: 1, headerAlign: 'center', align: 'center' },
-        { field: 'remarks', headerName: keyToHebrew['remarks'], flex: 1, headerAlign: 'center', align: 'center' },
-      ];
-
-      setColumns(newColumns);
-    }
+    const items = borrowingStore.getborrowingList;
+    console.log('Borrowing Items:', items);
+    setBorrowingItems(items);
   }, [borrowingStore.borrowingList]);
 
+  useEffect(() => {
+    const items = filterItems(toJS(borrowingStore.borrowingList));
+    console.log('Filtered Items:', items);
+    setFilteredItems(items);
+  }, [borrowingItems, filterType]);
+
+  const filterItems = (items) => {
+    debugger
+    // const JSitems=toJS(borrowingItems);
+    // console.log("borItems:",JSitems)
+    if (!items) {
+      return [];
+    }
+    if (filterType === "all") {
+      return items;
+    }
+    if (filterType === "book") {
+      return items.filter((item) => item.itemType === TypeEnum.BOOK);
+    }
+    if (filterType === 'object') {
+    return items.filter((item) =>item.itemType === TypeEnum.PHYSICALITEM);
+    }
+      return items.filter((item) => item.itemType === TypeEnum.FILE);
+  };
+
+  const handleSearch = (searchTerm) => {
+    const filtered = borrowingStore.borrowingList.filter((item) =>
+      item.itemTitle.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    setFilteredItems(filterItems(filtered));
+  };
+
+  const columns = [
+    { field: 'id', headerName: keyToHebrew['id'], flex: 1, headerAlign: 'center', align: 'center' },
+    { field: 'date', headerName: keyToHebrew['date'], flex: 1, headerAlign: 'center', align: 'center' },
+    { field: 'itemTitle', headerName: keyToHebrew['itemTitle'], flex: 1, headerAlign: 'center', align: 'center' },
+    {
+      field: 'itemType', headerName: keyToHebrew['itemType'],
+      renderCell: (params) => {
+        switch (params.row.itemType) {
+          case 'book': return <MenuBookRoundedIcon sx={{ color: "#0D1E46" }}/>
+          case 'file': return <TextSnippetRoundedIcon sx={{ color: "#0D1E46" }}/>
+          case 'physical': <CategoryIcon sx={{ color: "#0D1E46" }}/>
+        }
+      },
+      flex: 1,
+      headerAlign: 'center',
+      align: 'center'
+    },
+    {
+      field: 'StudentFullName',
+      headerName: keyToHebrew['StudentFullName'],
+      renderCell: (params) => {
+        const fname = params.row.studentFname || '';
+        const sname = params.row.studentSname || '';
+        return (
+          <div style={{ display: 'flex', alignItems: 'center', position: 'relative' }}>
+            <span style={{ marginRight: 40 }}>{`${fname} ${sname}`.trim()}</span>
+            <Tooltip title="לפרטים נוספים" arrow>
+              <IconButton
+                onClick={() => setStudentDetails(params.row)}
+                style={{ position: 'absolute', right: 0 }}
+              >
+                <CallMissedOutgoingIcon />
+              </IconButton>
+            </Tooltip>
+          </div>
+        );
+      },
+      flex: 1,
+      headerAlign: 'center',
+      align: 'center'
+    },
+    {
+      field: 'LibrarianFullName',
+      headerName: keyToHebrew['LibrarianFullName'],
+      renderCell: (params) => {
+        const fname = params.row.librarianFname || '';
+        const sname = params.row.librarianSname || '';
+        return (
+          <div style={{ display: 'flex', alignItems: 'center', position: 'relative' }}>
+            <span style={{ marginRight: 40 }}>{`${fname} ${sname}`.trim()}</span>
+            <Tooltip title="לפרטים נוספים" arrow>
+              <IconButton
+                onClick={() => setLibrarianDetails(params.row)}
+                style={{ position: 'absolute', right: 0 }}
+              >
+                <CallMissedOutgoingIcon />
+              </IconButton>
+            </Tooltip>
+          </div>
+        );
+      },
+      flex: 1,
+      headerAlign: 'center',
+      align: 'center'
+    },
+    { field: 'amount', headerName: keyToHebrew['amount'],
+    renderCell: (params) => {
+     if(!params.row.amount)
+     {
+      return 'לא מוגדרת כמות'
+     }
+    },
+     flex: 1, headerAlign: 'center', align: 'center' },
+    { field: 'remarks', headerName: keyToHebrew['remarks'], flex: 1, headerAlign: 'center', align: 'center' },
+  ];
 
   return (
-    <div style={{ height: 400, width: '100%', direction: 'rtl' }}>
+  loading?(
+    <Box
+    display="flex"
+    justifyContent="center"
+    alignItems="center"
+    height="50vh"
+  >
+    <CircularProgress />
+  </Box>
+  ):
+    (<div style={{ height: 400, width: '100%', direction: 'rtl' }}>
+         <Grid
+          container
+          spacing={2}
+          sx={{ backgroundColor: "#0D1E46", padding: 2 }}
+        >
+          <Grid
+            item
+            xs={6}
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Box
+              sx={{
+                width: "100%",
+                maxWidth: 600,
+                "& .MuiTabs-indicator": {
+                  backgroundColor: "#FFD700",
+                },
+                "& .MuiTab-root": {
+                  color: "#dcdcdc",
+                  "&.Mui-selected": {
+                    color: "#FFD700",
+                  },
+                },
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <Tabs
+                onChange={(e, newValue) => {
+                  setFilterType(newValue);
+                }}
+                value={filterType}
+                aria-label="Tabs where selection follows focus"
+                selectionFollowsFocus
+              >
+                <Tab label="הכל" value="all" />
+                <Tab label="ספרים" value="book" />
+                <Tab label="קבצים" value="file" />
+                <Tab label="חפצים" value="object" />
+              </Tabs>
+            </Box>
+          </Grid>
+          <Grid
+            item
+            xs={6}
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <CacheProvider value={cacheRtl}>
+              <ItemSearch onSearch={handleSearch} />
+            </CacheProvider>
+          </Grid>
+        </Grid>
       <DataGrid
-        rows={borrowingStore.borrowingList}
+        rows={filteredItems}
         columns={columns}
         localeText={localeText}
         initialState={{
@@ -220,11 +323,11 @@ const DataTable = observer(() => {
           student={studentDetails}
         />
       )}
-    </div>
+    </div>)
   );
 });
 
-export default DataTable;
+export default BorrowingTable;
 
 
 
--- src/components/item/item-list.jsx
+++ src/components/item/item-list.jsx
@@ -32,7 +32,38 @@ import StarIcon from '@mui/icons-material/Star';
 import StarBorderIcon from '@mui/icons-material/StarBorder';
 
 import { LevelEnum } from "../Enums";
-
+export const localeText = {
+  // תרגום של אפשרויות המיון והפילטור לעברית
+  columnMenuSortAsc: "מיון לפי סדר עולה",
+  columnMenuSortDesc: "מיון לפי סדר יורד",
+  columnMenuFilter: "סינון",
+  columnMenuHideColumn: "הסתר עמודה",
+  columnMenuUnsort: "בטל מיון",
+  noRowsLabel: "אין פריטים להצגה",
+  columnMenuManageColumns: "ניהול עמודות",
+  filterPanelAddFilter: "הוסף מסנן",
+  filterPanelDeleteIconLabel: "מחק",
+  filterPanelLinkOperator: "אופרטור לוגי",
+  filterPanelOperators: "אופרטור",
+  filterPanelOperatorAnd: "וגם",
+  filterPanelOperatorOr: "או",
+  filterPanelColumns: "עמודות",
+  filterPanelInputLabel: "ערך",
+  filterPanelInputPlaceholder: "סנן ערך",
+  filterOperatorContains: "מכיל",
+  filterOperatorEquals: "שווה",
+  filterOperatorStartsWith: "מתחיל ב",
+  filterOperatorEndsWith: "מסתיים ב",
+  filterOperatorIs: "הוא",
+  filterOperatorNot: "אינו",
+  filterOperatorAfter: "אחרי",
+  filterOperatorOnOrAfter: "ב או אחרי",
+  filterOperatorBefore: "לפני",
+  filterOperatorOnOrBefore: "ב או לפני",
+  filterOperatorIsEmpty: "ריק",
+  filterOperatorIsNotEmpty: "אינו ריק",
+  filterOperatorIsAnyOf: "הוא אחד מ",
+};
 const DataTable = observer(() => {
   const [deleteItem, setDeleteItem] = useState(null);
   const [deleteTag, setDeleteTag] = useState(null);
@@ -410,38 +441,7 @@ const DataTable = observer(() => {
     }
   };
 
-  const localeText = {
-    // תרגום של אפשרויות המיון והפילטור לעברית
-    columnMenuSortAsc: "מיון לפי סדר עולה",
-    columnMenuSortDesc: "מיון לפי סדר יורד",
-    columnMenuFilter: "סינון",
-    columnMenuHideColumn: "הסתר עמודה",
-    columnMenuUnsort: "בטל מיון",
-    noRowsLabel: "אין פריטים להצגה",
-    columnMenuManageColumns: "ניהול עמודות",
-    filterPanelAddFilter: "הוסף מסנן",
-    filterPanelDeleteIconLabel: "מחק",
-    filterPanelLinkOperator: "אופרטור לוגי",
-    filterPanelOperators: "אופרטור",
-    filterPanelOperatorAnd: "וגם",
-    filterPanelOperatorOr: "או",
-    filterPanelColumns: "עמודות",
-    filterPanelInputLabel: "ערך",
-    filterPanelInputPlaceholder: "סנן ערך",
-    filterOperatorContains: "מכיל",
-    filterOperatorEquals: "שווה",
-    filterOperatorStartsWith: "מתחיל ב",
-    filterOperatorEndsWith: "מסתיים ב",
-    filterOperatorIs: "הוא",
-    filterOperatorNot: "אינו",
-    filterOperatorAfter: "אחרי",
-    filterOperatorOnOrAfter: "ב או אחרי",
-    filterOperatorBefore: "לפני",
-    filterOperatorOnOrBefore: "ב או לפני",
-    filterOperatorIsEmpty: "ריק",
-    filterOperatorIsNotEmpty: "אינו ריק",
-    filterOperatorIsAnyOf: "הוא אחד מ",
-  };
+  
   const columns = [
     {
       field: "checkbox",
